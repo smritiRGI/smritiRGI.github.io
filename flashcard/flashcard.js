441c71f6349c02a5de1747a71ce2734675fbbf0e
Rastogi <Smriti.Rastogi@shell.com>
2020-10-13 20:09:47 +0530

    flashcard files changed

--- flashcard/flashcard.js
+++ flashcard/flashcard.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class flashCard {
     constructor(question,answer){
         this.question = question;
@@ -46,52 +45,3 @@ let submitQuestion = ()=>{
   const CARD = new flashCard(QUESTION,ANSWER);
   localStorage.setItem(QUESTION,ANSWER);
 }
-=======
-class flashCard {
-    constructor(question,answer){
-        this.question = question;
-        this.answer = answer;
-    }
-}
-
-let displaycards = document.getElementById("displaycards");
-
-for(let i=0;i<localStorage.length;i++){
-  let key = localStorage.key(i);
-  displaycards.innerHTML += `
-    <br>
-    <div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h6 class="card-text">${key}</h6>
-      <a href="#" onclick='displayAnswer("${i}")' class="card-link">Show/Hide Answer</a>
-      <h5 id=${i} class="card-text">${localStorage.getItem(key)}</h5>
-      <br>
-      <button class="btn btn-primary mt-2" onclick='deleteQuestion("${key}")'>Delete</button>
-    </div>
-    </div>`
-}
-
-let deleteQuestion =(key)=>{
-  localStorage.removeItem(key);
-  window.location.reload();
-}
-
-let displayAnswer =(id)=>{
-    let element = document.getElementById(id);
-    if(element.style.display == "none")
-      element.style.display = "block";
-    else
-      element.style.display = "none";
-}
-
-let addQuestion= ()=>{
-  document.getElementsByClassName('addquestion')[0].style.display =  "inline";
-}
-
-let submitQuestion = ()=>{
-  const QUESTION = document.forms["addquestion"]["question"].value;
-  const ANSWER = document.forms["addquestion"]["answer"].value;
-  const CARD = new flashCard(QUESTION,ANSWER);
-  localStorage.setItem(QUESTION,ANSWER);
-}
->>>>>>> e1d8d8a86d7c07e71c88b56fbb5aa7b0799d91e6
